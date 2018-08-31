f1fdfe388b2bc35a6801595cc17333eab9a6d5db
taejin.seo <happyasaclam@naver.com>
2018-08-31 21:00:28 +0900

    conflict1 file modify

--- happyasaclam/HelloWorldConflict.java
+++ happyasaclam/HelloWorldConflict.java
@@ -1,5 +1,6 @@
 public class HelloWorldConflict {
    public static void main(String[] args) {
-      System.out.println("Hello, World!");
+      System.out.println("This branch is conflict1");
+      System.out.println("Hello, World! conflict1");
    }
 }
