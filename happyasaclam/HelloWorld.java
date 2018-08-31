462044399c5ab13ab488ec792f2a4d47950bbea1
taejin.seo <happyasaclam@naver.com>
2018-08-31 17:19:15 +0900

    코드리뷰시항 적용 - 메서드 주석 추가

--- happyasaclam/HelloWorld.java
+++ happyasaclam/HelloWorld.java
@@ -5,6 +5,14 @@
  * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 public class HelloWorld {
+   /**
+   * Prints out "Hello World" 
+   * and the command line arguments.
+   * @param arg A string array containing 
+   * the command line arguments.
+   * @exception Any exception
+   * @return No return value.
+   */ 
    public static void main(String[] args) {
       System.out.println("Hello, World!");
    }
