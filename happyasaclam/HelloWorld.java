11ff7e6ded419031da5888b2530d220daabd8709
taejin.seo <happyasaclam@naver.com>
2018-08-31 17:14:25 +0900

    코드리뷰시항 적용 - 클래스 주석 추가

--- happyasaclam/HelloWorld.java
+++ happyasaclam/HelloWorld.java
@@ -1,3 +1,9 @@
+/*
+ * @(#)HeeloWorld.java $version 2018. 8. 1
+ *
+ * Copyright 2018 NHN Corp. All rights Reserved.
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 public class HelloWorld {
    public static void main(String[] args) {
       System.out.println("Hello, World!");
