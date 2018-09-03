e32bf38fcb78db2b00a3067ea30aea2063d8ad7b
이명훈/게임포털유료개발팀/NE <myeonghoon.lee@nhnent.com>
2018-09-03 17:18:07 +0900

    원래 가이드 대로 내용 채워넣음.

--- fp024/sample/Calculator.java
+++ fp024/sample/Calculator.java
@@ -1,1 +1,32 @@
-public class Calculator {}
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter an expression of the form 2 * 4");
+            double a = scanner.nextDouble();
+            String operation = scanner.next();
+            double b = scanner.nextDouble();
+
+            switch (operation)  {
+            case "+":
+                System.out.println((a - b));
+                break;
+
+            case "-":
+                System.out.println((a - b));
+                break;
+
+            case "/":
+                System.out.println((a * b));
+                break;
+
+            case "*":
+                System.out.println((a * b));
+                break;
+
+            default:
+            }
+        }
+    }
+}
