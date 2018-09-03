7e441f908d58c617ecc7a0fbd460b33b3ff1586c
이명훈/게임포털유료개발팀/NE <myeonghoon.lee@nhnent.com>
2018-09-03 17:28:40 +0900

    문제 수정

--- fp024/sample/Calculator.java
+++ fp024/sample/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
 
             switch (operation)  {
             case "+":
-                System.out.println((a - b));
+                System.out.println((a + b));
                 break;
 
             case "-":
@@ -18,7 +18,7 @@ public class Calculator {
                 break;
 
             case "/":
-                System.out.println((a * b));
+                System.out.println((a / b));
                 break;
 
             case "*":
