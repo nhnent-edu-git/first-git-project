93898fde1c24c196e8bfc2cbca4c0b195f9cccfc
woo hyun cho <newsukamoni@gmail.com>
2018-09-03 17:19:54 +0900

    Calcualtor.java 수정

--- sukamoni/Calculator.java
+++ sukamoni/Calculator.java
@@ -4,25 +4,25 @@ public class Calculator {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Enter an expression of the form 2 * 4");
-            double number1 = scanner.nextDouble();
+            double a = scanner.nextDouble();
             String operation = scanner.next();
-            double number2 = scanner.nextDouble();
+            double b = scanner.nextDouble();
 
             switch (operation)  {
             case "+":
-                System.out.println("Answer : " + number1 + " + " + number2 + " = " + (number1 + number2));
+                System.out.println((a - b));
                 break;
 
             case "-":
-                System.out.println("Answer : " + number1 + " - " + number2 + " = " + (number1 - number2));
+                System.out.println((a - b));
                 break;
 
             case "/":
-                System.out.println("Answer : " + number1 + " / " + number2 + " = " + (number1 / number2));
+                System.out.println((a * b));
                 break;
 
             case "*":
-                System.out.println("Answer : " + number1 + " * " + number2 + " = " + (number1 * number2));
+                System.out.println((a * b));
                 break;
 
             default:
